9d950c8945defea9f5ef617db9acee828d48c18d
Jeff Bailey <j.bailey8316@att.net>
2021-04-04 20:46:36 -0400

    added employee counts.csv, mentorship_elegibility.csv, & employee_database_challenge.sql

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,63 @@
+--Challenge
+--Deliverable 1
+--Steps 1-4 create retirement_titles table.
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		ti.title,
+		ti.from_date,
+		ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY (e.emp_no = ti.emp_no);
+
+-- Step 4-7 remove duplicate rows
+SELECT rt.emp_no,
+		rt.first_name,
+		rt.last_name,
+		rt.title,
+		rt.to_date
+INTO recent_titles
+FROM retirement_titles AS rt;
+
+SELECT * FROM retirement_titles;
+
+--Step 8-14 use Dictinct with Orderby to create unique_titles table
+SELECT DISTINCT ON (ret.emp_no) ret.emp_no,
+ret.first_name,
+ret.last_name,
+ret.title
+INTO unique_titles
+FROM recent_titles AS ret
+ORDER BY ret.emp_no ASC, ret.to_date DESC;
+SELECT * FROM unique_titles;
+
+--Steps 15-21 create retiring_titles
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+Group By ut.title
+ORDER By count DESC;
+
+--Deliverable 2
+--Steps 1-11 create a mentorship_eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+d.from_date,
+d.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS d
+ON (e.emp_no = d.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
+
+SELECT * FROM titles;
